49b85a5a40c4c927e57f7ffa834280db0e9313e7
blacksniper786 <docker261@gmail.com>
2022-10-18 17:47:39 +0100

    new

---
+++ Tp-01/Test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Test {
+	public static void main(String arg[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : " + str);
+	}
+
+}
